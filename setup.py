a6994688548e1b24ba630184025e0dacb026a214
Eugenio Paolantonio (g7) <me@medesimo.eu>
2015-04-02 17:49:18 +0200

    0.90.1
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- setup.py
+++ setup.py
@@ -169,7 +169,7 @@ setup(
 		"install": CustomInstall
 	},
 	name=APP_NAME,
-	version='0.90',
+	version='0.90.1',
 	description='User management daemon.',
 	author='Eugenio Paolantonio',
 	author_email='me@medesimo.eu',
